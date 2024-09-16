3b09ea118a774d39f9ab6090d161fea3417fb7f4
mr-roko <rbbagyei@gmail.com>
2024-09-16 18:07:44 +0000

    Exercise 5.7 - Blog List Frontend: each blog now has a show or hide button that shows more information about the blog and the user who added the blog.

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ const App = () => {
 
   const displayBlogs = () => {
     return blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} />
+      <Blog key={blog.id} blog={blog} user={user.name} />
     )
   }
 
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+const Blog = ({ blog, user }) => {
+const [showAllInfo, setShowAllInfo] = useState(false)
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+const toggleShowAllInfo = () => {
+  setShowAllInfo(!showAllInfo)
+}
+
+
+  return (
+
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleShowAllInfo}>{showAllInfo ? 'Hide' : 'Show'}</button>
+      </div>
+      {showAllInfo
+        ? <div>
+          <div>{blog.url}</div>
+          <div>{blog.likes} <button>likes</button></div>
+          <div>{user}</div>
+        </div>
+        : null
+        }
+       
+  </div>
+)}
 export default Blog
